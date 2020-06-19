c619b5f9d3c919e2f96ee4be54ad4cc7b171b77e
TobyChen0106 <awesdjzo@gmail.com>
2020-06-20 04:08:15 +0800

    add ip feature

--- src/route/api.js
+++ src/route/api.js
@@ -4,9 +4,11 @@ const UserContact = require('../models/UserContact');
 
 router.post('/save-user-contact', (req, res) => {
     const newdata = req.body;
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const newContact = new UserContact(newdata);
+    newContact.ip = ip;
     newContact.save().then((data) => {
-        console.log(`[User Contact!] <${data.time}> ${data.userEmail}: ${data.userInfo}`);
+        console.log(`[User Contact!] <${data.time}> ${data.userEmail}: [${data.userInfo}] at ${data.ip}`);
         res.json("OK");
     }).catch(function (error) {
         console.log("[Error] " + error);
